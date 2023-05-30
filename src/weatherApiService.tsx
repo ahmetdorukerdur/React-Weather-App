036467f1bdb8213f0fad1a40b078499a87c5bc6e
Doruk Erdur <Doruk.Erdur@fups.com>
2023-05-30 15:21:21 +0300

    adding states and usestates

--- src/weatherApiService.tsx
+++ src/weatherApiService.tsx
@@ -1,5 +1,5 @@
 const API_KEY = '42b50aa43934d83a0bd96dc96cc7207a';
-const iconURL = (iconId: any) => `http://openweathermap.org/img/wn/${iconId}04d@2x.png`;
+const iconURL = (iconId: any) => `http://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 const getWeatherData = async (city: any, units = 'metric') => {
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
